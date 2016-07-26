fe147a8094f3ae65aa8837593f2f6130c8fb0045
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-26 18:49:58 -0400

    modified how trainers' pokemons are listed, and how pokemons' trainers are listed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@ require_relative '../models/trainer'
 Trainer.destroy_all
 Pokemon.destroy_all
 
-Pokemon.create(name: "Catturd", cp: rand(800), poke_type: "catatonic", img_url: "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg")
-Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", img_url: "https://img.pokemondb.net/artwork/charmander.jpg")
-Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg")
-Pokemon.create(name: "Pikachu", cp: rand(800), poke_type: "lightning", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg")
-Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg")
-Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg")
-Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
-Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
-Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
+Pokemon.create(name: "Catturd", cp: rand(800), poke_type: "catatonic", img_url: "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg", trainer_id: 1)
+Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", img_url: "https://img.pokemondb.net/artwork/charmander.jpg", trainer_id: 1)
+Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg", trainer_id: 1)
+Pokemon.create(name: "Pikachu", cp: rand(800), poke_type: "lightning", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg", trainer_id: 2)
+Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg", trainer_id: 2)
+Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg", trainer_id: 2)
+Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg", trainer_id: 3)
+Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg", trainer_id: 3)
+Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg", trainer_id: 3)
 
-Trainer.create(name: "Stan", level: 1, img_url: "https://pbs.twimg.com/profile_images/656816032930119680/52m1eugJ.jpg")
-Trainer.create(name: "Janice", level: 2, img_url: "https://pbs.twimg.com/profile_images/757169060035657728/dME8Kj6M.jpg")
-Trainer.create(name: "Damien", level: 2, img_url: "https://pbs.twimg.com/profile_images/555533496676925440/9GmtXem_.jpeg")
+Trainer.create(name: "Stan", level: 5, img_url: "https://pbs.twimg.com/profile_images/656816032930119680/52m1eugJ.jpg")
+Trainer.create(name: "Janice", level: 22, img_url: "https://pbs.twimg.com/profile_images/757169060035657728/dME8Kj6M.jpg")
+Trainer.create(name: "Damien", level: 0, img_url: "https://pbs.twimg.com/profile_images/555533496676925440/9GmtXem_.jpeg")
